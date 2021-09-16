a4a9f5beb242b8e776bd1ad4c858e6d4a6cd5fb1
Tomasz Master <tsierzputowski@op.pl>
2021-09-16 16:10:34 +0200

    admin login, admin news

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: "mongodb+srv://admin:9CM1lbZnOqvaThCD@cluster0.gj7qp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    keySession: ["Twojklucz"],
+    maxAgeSession: 24 * 60 * 60 * 1000,
+};
---
+++ models/news.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const newsSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, 'pole tytuł jest wymagane']
+    },
+    description: {
+        type: String,
+        required: [true, 'pole opis jest wymagane']
+    },
+
+    created: {
+        type: Date,
+        default: Date.now
+    },
+
+});
+
+module.exports = mongoose.model("News", newsSchema)
