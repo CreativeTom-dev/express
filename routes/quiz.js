010149c64070f38ba927131c71b7df007874d951
Tomasz Master <tsierzputowski@op.pl>
2021-09-17 11:29:46 +0200

    quiz form

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,10 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const Quiz = require('../models/quiz')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('quiz', {
-        title: 'quiz'
+
+    const show = !req.session.vote;
+
+    Quiz.find({}, (err, data) => {
+        let sum = 0;
+        data.forEach((item) => {
+            sum += item.vote;
+        })
+
+
+        res.render('quiz', {
+            title: 'quiz',
+            data,
+            show,
+            sum
+        });
+    })
+
+    // dodawanie pojedynczego rekordu
+
+    // new Quiz({
+    //     title: 'pytanie 1',
+    //     vote: 0
+    // }).save()
+
+
+});
+
+router.post('/', (req, res, next) => {
+
+    const id = req.body.quiz
+
+    Quiz.findOne({
+        _id: id
+    }, (err, data) => {
+
+        data.vote = data.vote + 1;
+        data.save((err) => {
+            req.session.vote = 1;
+            res.redirect('/quiz')
+        });
+
     });
 });
 
